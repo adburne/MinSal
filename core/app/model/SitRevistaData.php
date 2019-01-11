0a9a4a7932d34895bed2dc45b0781379db443be6
adburne <adburne@amr.org.ar>
2019-01-11 01:04:40 -0300

    Carga inicial

---
+++ core/app/model/SitRevistaData.php
@@ -0,0 +1,71 @@
+<?php
+class SitRevistaData {
+	public static $tablename = "sit_revista";
+
+	public function __constructor(){
+		$this->name = "";
+		$this->created_at = "NOW()";
+	}
+
+	public function add(){
+		$sql = "insert into ".self::$tablename." (name,created_at) ";
+		$sql .= "value (\"$this->name\",$this->created_at)";
+		Executor::doit($sql);
+	}
+
+	public static function delById($id){
+		$sql = "delete from ".self::$tablename." where id=$id";
+		Executor::doit($sql);
+	}
+	public function del(){
+		$sql = "delete from ".self::$tablename." where id=$this->id";
+		Executor::doit($sql);
+	}
+
+// partiendo de que ya tenemos creado un objecto CategoryData previamente utilizamos el contexto
+	public function update(){
+		$sql = "update ".self::$tablename." set name=\"$this->name\" where id=$this->id";
+		Executor::doit($sql);
+	}
+
+	public static function getById($id){
+		$sql = "select * from ".self::$tablename." where id=$id";
+		$query = Executor::doit($sql);
+		return Model::one($query[0],new SitRevistaData());
+	}
+
+	public static function getAll(){
+		$sql = "select * from ".self::$tablename;
+		$query = Executor::doit($sql);
+		$array = array();
+		$cnt = 0;
+		while($r = $query[0]->fetch_array()){
+			$array[$cnt] = new SitRevistaData();
+			$array[$cnt]->id = $r['id'];
+			$array[$cnt]->name = $r['name'];
+			$array[$cnt]->created_at = $r['created_at'];
+			$cnt++;
+		}
+		return $array;
+	}
+
+
+	public static function getLike($q){
+		$sql = "select * from ".self::$tablename." where name like '%$q%'";
+		$query = Executor::doit($sql);
+		$array = array();
+		$cnt = 0;
+		while($r = $query[0]->fetch_array()){
+			$array[$cnt] = new SitRevistaData();
+			$array[$cnt]->id = $r['id'];
+			$array[$cnt]->name = $r['name'];
+			$array[$cnt]->created_at = $r['created_at'];
+			$cnt++;
+		}
+		return $array;
+	}
+
+
+}
+
+?>
