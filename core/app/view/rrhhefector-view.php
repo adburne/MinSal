d8080e3058f295eabb4790a11bb36544efec51af
adburne <adburne@amr.org.ar>
2019-01-11 12:33:02 -0300

    Primera versión que muestra la info por efector

--- core/app/view/rrhhefector-view.php
+++ core/app/view/rrhhefector-view.php
@@ -1,94 +1,91 @@
 <?php
-
-$page = 1;
-if(isset($_GET["page"])){
-	$page=$_GET["page"];
-}
-$limit=20;
-if(isset($_GET["limit"]) && $_GET["limit"]!="" && $_GET["limit"]!=$limit){
-	$limit=$_GET["limit"];
-}
-
-$Efectores = EfectorData::getLike($_GET["efector"]);
-
-if(count($Efectores)>0){
-
-if($page==1){
-$curr_Efector = EfectorData::getLikeByPage($_GET["efector"],$Efectores[0]->id,$limit);
-}else{
-$curr_Efector = EfectorData::getLikeByPage($_GET["efector"],$Efectores[($page-1)*$limit]->id,$limit);
-
-}
-$npaginas = floor(count($Efectores)/$limit);
- $spaginas = count($Efectores)%$limit;
-
-if($spaginas>0){ $npaginas++;}
-
-	?>
-
-	<h3>Pagina <?php echo $page." de ".$npaginas; ?></h3>
-<div class="btn-group pull-right">
-<?php
-$px=$page-1;
-if($px>0):
+$efectores = EfectorData::getAllByUserId($_SESSION["user_id"]);
+$servrrhhs = ServRRHHData::getAll();
 ?>
-<a class="btn btn-sm btn-default" href="<?php echo "index.php?view=Efectores&limit=$limit&page=".($px); ?>"><i class="glyphicon glyphicon-chevron-left"></i> Atras </a>
-<?php endif; ?>
 
-<?php 
-$px=$page+1;
-if($px<=$npaginas):
-?>
-<a class="btn btn-sm btn-default" href="<?php echo "index.php?view=Efectores&limit=$limit&page=".($px); ?>">Adelante <i class="glyphicon glyphicon-chevron-right"></i></a>
-<?php endif; ?>
-</div>
+<h3>RRHH por Efector</h3>
+<!-- <div class="btn-group pull-right"></div> -->
 <div class="clearfix"></div>
-<br><table class="table table-bordered table-hover">
-	<thead>
-		<th>C&oacute;digo</th>
-		<th>Denominaci&oacute;n</th>
-		<th></th>
-	</thead>
-	<?php foreach($curr_Efector as $Efector):?>
-	<tr>
-		<td><?php echo $Efector->codest; ?></td>
-		<td><?php echo $Efector->nomest; ?></td>
-		
 
-		<td style="width:70px;">
-		<a href="index.php?view=editefector&id=<?php echo $Efector->id; ?>" class="btn btn-xs btn-warning"><i class="glyphicon glyphicon-pencil"></i></a>
-		<!-- <a href="index.php?view=delefector&id=<?php echo $Efector->id; ?>" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a> -->
-		</td>
-	</tr>
-	<?php endforeach;?>
+<br>
+
+<form method="post" action="<?php echo htmlspecialchars("/MinSal/?view=rrhhefector");?>">
+
+<table class="table table-bordered table-hover">
+
+ <!-- Efector -->
+ <tr>
+  <td>
+   <label class="control-label">Seleccione el efector</label>
+  </td>
+  <td>
+   <select name="codefe" class="form-control">
+   <option value="">-- Seleccione la opci&oacute;n --</option>
+   <?php foreach($efectores as $efector):?>
+    <option value="<?php echo $efector->codest;?>" <?php if(isset($_POST["codefe"])){if($_POST["codefe"]!=null && $_POST["codefe"]==$efector->codest){ echo "selected";};}?>><?php echo $efector->nomest;?></option>
+   <?php endforeach;?>
+   </select>
+  </td>
+ </tr>
+
+ <!-- Servicio -->
+ <tr>
+  <td>
+   <label class="control-label">Seleccione el servicio</label>
+  </td>
+  <td>
+   <select name="codservrrhh" class="form-control">
+   <option value="">-- Todos --</option>
+   <?php foreach($servrrhhs as $servrrhh):?>
+   <option value="<?php echo $servrrhh->id;?>" <?php if(isset($_POST["codservrrhh"])){if($_POST["codservrrhh"]!=null && $_POST["codservrrhh"]==$servrrhh->id){ echo "selected";};}?>><?php echo $servrrhh->name;?></option>
+   <?php endforeach;?>
+   </select>    
+  </td>
+  <td style="width:300px;">
+  </td>
+ </tr>
+
 </table>
-<div class="btn-group pull-right">
-<?php
-
-for($i=0;$i<$npaginas;$i++){
-	echo "<a href='index.php?view=Efector&limit=$limit&page=".($i+1)."' class='btn btn-default btn-sm'>".($i+1)."</a> ";
-}
-?>
-</div>
-<form class="form-inline">
-	<label for="limit">Límite</label>
-	<input type="hidden" name="view" value="Efector">
-	<input type="number" value=<?php echo $limit?> name="limit" style="width:80px;" class="form-control">
+<button type="submit" class="btn btn-primary"><i class="glyphicon glyphicon-search"></i> Mostrar resultados</button>
 </form>
 
-<div class="clearfix"></div>
-
-	<?php
-}else{
-	?>
-	<div class="jumbotron">
-		<h2>No hay efectores encontrados con los criterios ingresados</h2>
-		<p>Puede agregar una dando click en el boton <b>"Nuevo Efector"</b>.</p>
-	</div>
-	<?php
-}
-
+<?php 
+if(isset($_POST["codefe"])){
+$rrhhefectores = RRHHEfectorData::getByEfeServ($_POST["codefe"],$_POST["codservrrhh"]);
 ?>
-<br><br><br><br><br><br><br><br><br><br>
-	</div>
+
+<div class="row">
+ <div class="col-md-12">
+ <br><h3>N&oacute;mina de RRHH</h3>
+ </div>
 </div>
+
+<table class="table table-bordered table-hover">
+<thead>
+	<th>Documento</th>
+	<th>Nombre</th>
+	<th>Servicio</th>
+	<th>Sit.Revista</th>
+	<th>Horas</th>
+	<th>Observaciones</th>
+</thead>
+
+<?php foreach($rrhhefectores as $rrhh):?>
+<tr>
+ <td><?php echo $rrhh->nrodoc; ?></td>
+ <td><?php echo $rrhh->getRRHH()->nombre; ?></td>
+ <td><?php echo $rrhh->getServRRHH()->name; ?></td>
+ <td><?php echo $rrhh->getSitRev()->name; ?></td>
+ <td><?php echo $rrhh->carga_horaria; ?></td>
+ <td><?php echo $rrhh->observaciones; ?></td>
+</tr>
+
+<?php endforeach; ?>
+
+
+</table>
+	
+<?php 
+}
+?>
+
