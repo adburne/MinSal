35dc9afb1229cf1295e47e0bfd24b814ed9fac73
adburne <adburne@amr.org.ar>
2019-01-11 13:03:47 -0300

    GetAll devuelve solo los provinciales

--- core/app/model/EfectorData.php
+++ core/app/model/EfectorData.php
@@ -161,13 +161,10 @@ class EfectorData {
 		$sql = "select * from ".self::$tablename." where id=$id";
 		$query = Executor::doit($sql);
 		return Model::one($query[0],new EfectorData());
-
 	}
 
-
-
 	public static function getAll(){
-		$sql = "select * from ".self::$tablename;
+		$sql = "select * from ".self::$tablename." WHERE codda=3";
 		$query = Executor::doit($sql);
 		return Model::many($query[0],new EfectorData());
 	}
