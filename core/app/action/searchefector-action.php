0697dfa06cbbf0b883a55905e1e323368e509f0f
adburne <adburne@amr.org.ar>
2019-02-04 22:38:50 -0300

    Se elimino la paginación

--- core/app/action/searchefector-action.php
+++ core/app/action/searchefector-action.php
@@ -1,47 +1,17 @@
 <?php
 
-$page = 1;
-if(isset($_GET["page"])){
-	$page=$_GET["page"];
-}
-$limit=20;
-if(isset($_GET["limit"]) && $_GET["limit"]!="" && $_GET["limit"]!=$limit){
-	$limit=$_GET["limit"];
+$get_efector="";
+if(isset($_GET["efector"]) && $_GET["efector"]!=""){
+	$get_efector=$_GET["efector"];
 }
 
-$Efectores = EfectorData::getLikeByUserId($_GET["efector"],$_SESSION["user_id"]);
+$Efectores = EfectorData::getLikeByUserId($get_efector,$_SESSION["user_id"]);
 
 if(count($Efectores)>0){
 
-if($page==1){
-$curr_Efector = EfectorData::getLikeByPageUserId($_GET["efector"],$Efectores[0]->id,$limit,$_SESSION["user_id"]);
-}else{
-$curr_Efector = EfectorData::getLikeByPageUserId($_GET["efector"],$Efectores[($page-1)*$limit]->id,$limit,$_SESSION["user_id"]);
-}
-
-$npaginas = floor(count($Efectores)/$limit);
-$spaginas = count($Efectores)%$limit;
-
-if($spaginas>0){ $npaginas++;}
-
-	?>
-
-	<h3>Pagina <?php echo $page." de ".$npaginas; ?></h3>
-<div class="btn-group pull-right">
-<?php
-$px=$page-1;
-if($px>0):
 ?>
-<a class="btn btn-sm btn-default" href="<?php echo "index.php?view=efectores&limit=$limit&page=".($px); ?>"><i class="glyphicon glyphicon-chevron-left"></i> Atras </a>
-<?php endif; ?>
 
-<?php 
-$px=$page+1;
-if($px<=$npaginas):
-?>
-<a class="btn btn-sm btn-default" href="<?php echo "index.php?view=efectores&limit=$limit&page=".($px); ?>">Adelante <i class="glyphicon glyphicon-chevron-right"></i></a>
-<?php endif; ?>
-</div>
+<div class="btn-group pull-right"></div>
 <div class="clearfix"></div>
 <br><table class="table table-bordered table-hover">
 	<thead>
@@ -49,12 +19,10 @@ if($px<=$npaginas):
 		<th>Denominaci&oacute;n</th>
 		<th></th>
 	</thead>
-	<?php foreach($curr_Efector as $Efector):?>
+	<?php foreach($Efectores as $Efector):?>
 	<tr>
 		<td style="width:10px;"><?php echo $Efector->codest; ?></td>
 		<td style="width:40px;"><?php echo $Efector->nomest; ?></td>
-		
-
 		<td style="width:40px;">
 		<a href="index.php?view=editefector&id=<?php echo $Efector->id; ?>" class="btn btn-xs btn-warning"><i class="glyphicon glyphicon-pencil"></i></a>
 		<!-- <a href="index.php?view=delefector&id=<?php echo $Efector->id; ?>" class="btn btn-xs btn-danger"><i class="fa fa-trash"></i></a> -->
@@ -62,23 +30,10 @@ if($px<=$npaginas):
 	</tr>
 	<?php endforeach;?>
 </table>
-<div class="btn-group pull-right">
-<?php
-
-for($i=0;$i<$npaginas;$i++){
-	echo "<a href='index.php?view=efectores&limit=$limit&page=".($i+1)."' class='btn btn-default btn-sm'>".($i+1)."</a> ";
-}
-?>
-</div>
-<form class="form-inline">
-	<label for="limit">Límite</label>
-	<input type="hidden" name="view" value="Efector">
-	<input type="number" value=<?php echo $limit?> name="limit" style="width:80px;" class="form-control">
-</form>
 
 <div class="clearfix"></div>
 
-	<?php
+<?php
 }else{
 	?>
 	<div class="jumbotron">
@@ -89,6 +44,5 @@ for($i=0;$i<$npaginas;$i++){
 }
 
 ?>
-<br><br><br><br><br><br><br><br><br><br>
 	</div>
 </div>
