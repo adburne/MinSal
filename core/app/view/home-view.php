9a7d6153b8c7767212bfaf9f034f682db9914d54
adburne <adburne@amr.org.ar>
2019-02-06 19:24:31 -0300

    Se eliminaron entradas innecesarias

--- core/app/view/home-view.php
+++ core/app/view/home-view.php
@@ -68,19 +68,6 @@
       <!-- /.row -->
 
 <div class="row">
-	<div class="col-md-12">
-<!-- <?php if($found):?>
-<div class="btn-group pull-right">
-  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-    <i class="fa fa-download"></i> Descargar <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" role="menu">
-    <li><a href="report/alerts-word.php">Word 2007 (.docx)</a></li>
-  </ul>
-</div>
-<?php endif;?> -->
-
-</div>
 <div class="clearfix"></div>
 
 <br><br><br><br><br><br><br><br><br><br>
