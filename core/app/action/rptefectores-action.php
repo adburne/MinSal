549dc52661493474c02ed137ef6f8b891c0548ec
adburne <adburne@amr.org.ar>
2019-02-14 20:11:57 -0300

    Se agregaron campo a la exportacion de datos

--- core/app/action/rptefectores-action.php
+++ core/app/action/rptefectores-action.php
@@ -4,14 +4,14 @@ $Efectores = EfectorData::getAllByUserId($_SESSION["user_id"]);
 
 $n = 0;
 foreach ($Efectores as $efector) {
- $arraydatos[$n]=array($efector->codest,$efector->nomest);
+ $arraydatos[$n]=array($efector->codest,$efector->nomest,$efector->domicilio,$efector->codloc,$efector->telefono,$efector->email);
  $n = ++$n;
 }
 
 
 $planilla = new Excel();
 
-$CellName = array('Codigo','Denominacion');
+$CellName = array('Codigo','Denominacion','Domicilio','Localidad','Teléfono','Email');
 
 $exportar = $planilla->exportExcel('Titulo', $CellName, 'Efectores', $arraydatos);
 
