de0d3b32039817bdc9d30fe891222406cf366755
adburne <adburne@amr.org.ar>
2019-02-14 20:12:49 -0300

    Se agregó formato a los títulos y cuerpo de las planillas

--- core/controller/Excel.php
+++ core/controller/Excel.php
@@ -13,13 +13,35 @@ class Excel
         $cellNum = count($expCellName);
         $dataNum = count($expTableData);
         $objPHPExcel = new PHPExcel();
+
+        $styleTitleArray = array(
+            'font'  => array(
+                'bold'  => true,
+                'color' => array('rgb' => '000000'),
+                'size'  => 10,
+                'name'  => 'Verdana'
+            ),
+            'alignment' => array(
+                'horizontal' => PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+            ));
+        $styleBodyArray = array(
+                'font'  => array(
+                    'bold'  => false,
+                    'color' => array('rgb' => '000000'),
+                    'size'  => 10,
+                    'name'  => 'Verdana'
+                ));
+            
         $cellName = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ');
         $objPHPExcel->getActiveSheet(0)->mergeCells('A1:' . $cellName[$cellNum - 1] . '1');
         for ($i = 0; $i < $cellNum; $i++) {
+            $objPHPExcel->getActiveSheet()->getStyle($cellName[$i].'2')->applyFromArray($styleTitleArray);
             $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellName[$i] . '2', $expCellName[$i]);
         }
         for ($i = 0; $i < $dataNum; $i++) {
             for ($j = 0; $j < $cellNum; $j++) {
+//                $objPHPExcel->getActiveSheet()->getStyle($cellName[$j].($i + 3).':'.$cellName[$j].($i + 3))->getFont()->setSize(10);
+                $objPHPExcel->getActiveSheet()->getStyle($cellName[$j].($i + 3))->applyFromArray($styleBodyArray);
                 $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellName[$j] . ($i + 3), $expTableData[$i][$j]);
 }
         }
